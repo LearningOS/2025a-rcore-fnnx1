da84e5ee7e80abd94051be63b80e70fbfa3c1c24
fnnx1 <FMxi666@outlook.com>
2025-12-11 18:07:43 +0800

    noted

--- os/src/config.rs
+++ os/src/config.rs
@@ -10,7 +10,7 @@ pub const KERNEL_STACK_SIZE: usize = 4096 * 2;
 pub const KERNEL_HEAP_SIZE: usize = 0x200_0000;
 
 /// page size : 4KB
-pub const PAGE_SIZE: usize = 0x1000;
+pub const PAGE_SIZE: usize = 0x1000;//d4096, b1_0000_0000_0000
 /// page size bits: 12
 pub const PAGE_SIZE_BITS: usize = 0xc;
 /// the max number of syscall
@@ -20,6 +20,6 @@ pub const TRAMPOLINE: usize = usize::MAX - PAGE_SIZE + 1;
 /// the virtual addr of trap context
 pub const TRAP_CONTEXT_BASE: usize = TRAMPOLINE - PAGE_SIZE;
 /// clock frequency
-pub const CLOCK_FREQ: usize = 12500000;
+pub const CLOCK_FREQ: usize = 12_500_000;//12.5MHz
 /// the physical memory end
-pub const MEMORY_END: usize = 0x88000000;
+pub const MEMORY_END: usize = 0x8800_0000;
